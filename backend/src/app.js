3d66313294ce7a675feadeb9318a2aebf0d46999
kunalrisaanva <kunalrisaanva12@gmail.com>
2025-12-05 16:31:15 +0530

    Enhance CORS configuration in app.js to specify allowed origins and methods

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser";
 import errorMiddleware from "./middleware/error.middleware.js";
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",                       // ❗ Any website can access your API
+  methods:["GET","POST","PUT","PATCH"]
+}));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
