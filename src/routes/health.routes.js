b6d5b287be1e3998fa77c0d64d2d3bf598c15416
Kunalll <106508267+kunalrisaanva@users.noreply.github.com>
2025-12-05 16:19:06 +0530

    Delete src directory

--- src/routes/health.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-
-router.route("/health-check").get((_, res) => res.json({msg:"Server is Healthy"}));
-
-export { router as healthRoutes };
