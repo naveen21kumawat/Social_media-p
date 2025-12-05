704d4207a27ae3bf4c4f3922443b0eb2352b0855
kunalrisaanva <kunalrisaanva12@gmail.com>
2025-12-05 16:20:53 +0530

    Simplify CORS configuration in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,7 @@ import cookieParser from "cookie-parser";
 import errorMiddleware from "./middleware/error.middleware.js";
 const app = express();
 
-app.use(
-  cors({
-    origin: (process.env.CORS_ORIGIN || "http://localhost:3000").split(','),
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
